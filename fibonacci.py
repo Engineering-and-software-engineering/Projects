07098b909049ace8af98026ae2c4a9c1d992b656
Jon-Paul Lindquist <jonpaul@lindquist.me>
2014-07-31 18:41:06 -0700

    Adding fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+def fibonnaciSequence(n):
+    assert n > 0, \ "Number must be greater than 0"
+
+    sequence = [1]  # Initialize sequence to 1
+    while len(sequence) < n:
+        if len(sequence) == 1:
+            # If the length of sequence is 1, append 1 to the end so the series is 1, 1 so far
+             sequence.append(1)
+        else:
+            # Add the previous 2 numbers in the series, and append it to the list
+            sequence.append(sequence[-1] + sequence[-2]) 
+    for i in range(len(sequence)):
+        # Convert the numbers to strings
+        sequence[i] = str(sequence[i])
+    # Return the sequence, separated by commas
+    return (', '.join(sequence))
+
+def main():
+    print(fibonnaciSequence(int(input('How many numbers do you need? '))))
+
+if __name__ == "__main__":
+    main()

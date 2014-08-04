1d47ee25c97eabdcb4b7d26659e6c8c9e1951690
Jon-Paul Lindquist <jonpaul@lindquist.me>
2014-08-03 19:04:36 -0700

    Added a 4th option to use the built in math library.  Completed Gregory–Leibniz series

--- Numbers/pi.py
+++ Numbers/pi.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import math
 
 """
 **Find PI to the Nth Digit**
@@ -8,6 +9,22 @@ that many decimal places. Keep a limit to how far the program will go.
 
 def gregory_leibniz_series(n):
     assert n > 0
+
+    pi = 1
+    limit = 10000
+    
+    for i in range(2, limit, 1):
+        pi += ((-1.0) ** (i + 1.0)) / (2.0 * i - 1.0)
+    pi = 4 * pi
+    print """
+        Gregory-Leibniz Series
+        pi = 4 (1 - (1/3) + (1/5) - (1/7) + ... (-1)^(i+1) / (2i -1))
+
+        Leibniz's formula converges extremely slowly and is fairly inaccurate
+        Calculating pi to 10 correct decimal places using direct summation
+        of the series requires about 5,000,000,000 terms
+        """        
+    return pi    
 
 def nilakantha_series(n):
     assert n > 0
@@ -19,6 +36,7 @@ def main():
     print "1) Gregory-Leibniz Series\n"
     print "2) Nilakantha Series\n"
     print "3) Chudnovsky Algorithm\n"
+    print "4) Built in math library\n"
 
     method = raw_input("Which method would you like to use? ")
     try:
@@ -36,12 +54,18 @@ def main():
     if n > 0:
         if method == 1:
             print "Calculating pi using the Gregory-Leibniz series..."
+            pi = gregory_leibniz_series(n)
         elif method == 2:
             print "Calculating pi using the Nilakantha series..."
         elif method == 3:
             print "Calculating pi using the Chudnovsky algorithm..."
+        elif method == 4:
+            print "Calculating pi using the built in math library..."
+            pi = math.pi
         else:
             print "Invalid option.  Exiting..."
+            exit(0)
+        print '%.*f' % (n, pi)
     else:
         print "You must enter a number larger than 0.  Try again..."
         main()

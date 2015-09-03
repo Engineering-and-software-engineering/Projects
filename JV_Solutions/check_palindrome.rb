38c158044217100fe8e1ae10b48ad751ae72dba3
pinkninjajess <jessica.valenti1@gmail.com>
2015-09-02 21:09:22 -0400

    Pig Latin Solution

---
+++ JV_Solutions/check_palindrome.rb
@@ -0,0 +1,23 @@
+class CheckPalindrome
+=begin
+Author: Jessica Valenti
+Date: September 2, 2015
+
+Script that determines whether a string is a palindrome.
+=end
+
+  def checkPalindrome(word)
+    reversedWord = word.reverse
+    if (word == reversedWord)
+      puts "#{word} is a palindrome!"
+    else
+      puts "#{word} is not a palindrome :("
+    end
+  end
+
+  puts "Which string would you like to check?"
+  word = gets.chomp.downcase
+
+  palindromeObject = CheckPalindrome.new
+  palindromeObject.checkPalindrome(word)
+end
